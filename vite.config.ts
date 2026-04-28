92d0a72c3afd2e67a87ef8357e3d9378469e2fcf
satishgouda405-hash <satishgouda405@gmail.com>
2026-04-28 10:04:42 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
